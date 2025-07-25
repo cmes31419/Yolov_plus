c20b47fb90ceb1119660856648147b658cfd212a
cmes31419 <cmes31419@gmail.com>
2025-07-25 13:34:22 +0800

    init

---
+++ run_demo.sh
@@ -0,0 +1,7 @@
+python vid_demo.py \
+    -f v++_base_decoupleReg.py \
+    -c v++_s.pth \
+    --path videos/test.mp4 \
+    --conf 0.25 --nms 0.5 --tsize 576 --save_result true
+
+#-f exps/yolov/yolov_s.py \
